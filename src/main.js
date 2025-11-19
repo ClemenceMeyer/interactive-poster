4acf32a3468bd7cd6172eb1a7e06423c86eb02d6
Clémence Meyer <clemence.meyer@gmail.com>
2025-11-19 12:23:02 +0100

    fix(handle scale & z-index)

--- src/main.js
+++ src/main.js
@@ -18,20 +18,28 @@ const planetsAnimInfo = {
   brittleHollow: {
     zIndex: [120, 40],
     duration: 10,
-    sizeModifier: 400
+    minY: 185.15303,
+    midY: 264.394565,
+    maxY: 343.6361,
+    scaleModifier: [0.5, 2]
   },
   timberHearth: {
     zIndex: [130, 30],
     duration: 15,
-    sizeModifier: 300
+    minY: 163.55428,
+    midY: 271.88205,
+    maxY: 380.20982,
+    scaleModifier: [0.25, 4]
   },
   giantsDeep: {
     zIndex: [140, 20],
     duration: 20,
-    sizeModifier: 200
+    minY: 174.31512,
+    midY: 288.22268,
+    maxY: 402.13024,
+    scaleModifier: [0.125, 8]
   },
 }
-
 
 const planetsOrbitsPath = {
   brittleHollow: {raw: orbitRawPathBrittleHollow, path: '#path-brittle-hollow'},
@@ -39,15 +47,25 @@ const planetsOrbitsPath = {
   giantsDeep: {raw: orbitRawPathGiantsDeep, path: '#path-giants-deep'}
 }
 
-const scaleModifier = function (scale, target) {
+const scaleMod = function (scale, target) {
   const pos = MotionPathPlugin.getPositionOnPath(planetsOrbitsPath[target.dataset.key].raw, this.ratio);
-  console.log(pos.y)
-  return pos.y / planetsAnimInfo[target.dataset.key].sizeModifier;
+  const animInfo = planetsAnimInfo[target.dataset.key]
+  if (pos.y <= animInfo.midY) {
+      // Map minY → midY   ↦   0.5 → 1
+        const t = (pos.y - animInfo.minY) / (animInfo.midY - animInfo.minY);
+        return animInfo.scaleModifier[0] + t * (1 - animInfo.scaleModifier[0]);
+    } else {
+        // Map midY → maxY   ↦   1 → 2
+        const t = (pos.y - animInfo.midY) / (animInfo.maxY - animInfo.midY);
+        return 1 + t * (animInfo.scaleModifier[1] - 1);
+    }
+  return 1;
 };
 
 const zIndexMod = function (scale, target) {
   const pos = MotionPathPlugin.getPositionOnPath(planetsOrbitsPath[target.dataset.key].raw, this.ratio);
-  return pos.y / planetsAnimInfo[target.dataset.key].sizeModifier;
+  const animInfo = planetsAnimInfo[target.dataset.key]
+  return pos.y > animInfo.midY ? animInfo.zIndex[0] : animInfo.zIndex[1];
 };
 
 const createOrbitForImg = (img) => {
@@ -65,8 +83,8 @@ const createOrbitForImg = (img) => {
       zIndex: 1,
       modifiers: {
         zIndex: zIndexMod,
-        scaleX: scaleModifier,
-        scaleY: scaleModifier
+        scaleX: scaleMod,
+        scaleY: scaleMod
       }
     },
     0
@@ -87,7 +105,7 @@ const resumeAnims = () => {
   })
 }
 
-/****************************/
+/************** setup **************/
 
 orbitImgs.forEach(img => {
   planetsTls[img.dataset.key] = createOrbitForImg(img)
